a5fee0058cec9bd5399b9e1ee7133417b522a519
pankaj suthar <146710838+Pankaj-suthar777@users.noreply.github.com>
2024-01-25 11:44:22 +0530

    Update cloudinaryConfig.js

--- cloudinaryConfig.js
+++ cloudinaryConfig.js
@@ -13,7 +13,8 @@ const storage = new CloudinaryStorage({
       folder: 'wanderlust_image_dev',
       allowerdFormats: [ 'png','jpg','jpeg'], // supports promises as well
     //   public_id: (req, file) => 'computed-filename-using-request',
-    // },
+    // 
+        },
   });
 
   module.exports = {
